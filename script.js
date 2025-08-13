e19cda236476648d3948f5ddeb55bb602f05fc33
Andre Henrique Silva Castro <andr.henrqui@gmail.com>
2025-08-13 02:50:18 -0300

    Finalizada pagina home

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ function logar(){
     var login = document.getElementById('login').value;
     var password = document.getElementById('password').value;
 
-    if(login == 'admin@teste.com' && password == 'admin@teste.com'){
+    if(login == 'timeitj@selbetti.com.br' && password == 'recruta7070'){
         alert('Sucesso');
         location.href = 'home.html'
     } else{
         alert('Usuário ou senha incorretos');
     }
+}
+
+function copiarComando(botao) {
+    const comando = botao.getAttribute("data-comando");
+    navigator.clipboard.writeText(comando).then(() => {
+        alert("Comando copiado: " + comando);
+    }).catch(err => {
+        alert("Erro ao copiar: " + err);
+    });
 }
