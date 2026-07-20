c0da767732c62c1ae920f1cfc904b75bcfeaa44b
Andre Henrique Silva Castro <71542547+andrhenrqui@users.noreply.github.com>
2026-07-20 09:18:22 -0300

    Atualizar o instalacao_ndd_agente.sh

--- instalacao_ndd_agente.sh
+++ instalacao_ndd_agente.sh
@@ -117,9 +117,9 @@ fi
 
 ### === ATUALIZAÇÃO DE PACOTES COM TRATAMENTO DE ERROS === ###
 echo "📡 Verificando atualização de pacotes configurados..."
+sudo dpkg --configure -a
 sudo systemctl stop cups-browsed.service
 sudo systemctl disable cups-browsed.service
-sudo dpkg --configure -a
 sudo apt-get install net-tools -y
 sudo systemctl restart cups
 if ! sudo apt-get update; then
